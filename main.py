84528e15fdcd97c6ec5f4ca17e46a8d48c95a65e
Eli McMakin <elimc184@outlook.com>
2022-05-20 11:48:23 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+###############################################################################
+#
+# Name: HelloDear
+# Purpose: Talk to scammers with an AI to waste their time.
+# Maintainer: Team HelloDear
+# Version: 0.1
+#
+# The admin's API info is located here. Your's will be different:
+# https://my.telegram.org/apps
+#
+# We use the following as a jump off point for our pretrained AI
+# https://www.thepythoncode.com/article/conversational-ai-chatbot-with-huggingface-transformers-in-python
+#
+# These are other AI options:
+# https://github.com/hyunwoongko/openchat
+# https://medium.com/ai-network/everyones-ai-explore-ai-model-2-chatbot-open-chat-3ef6667a4788
+#
+###############################################################################
+
