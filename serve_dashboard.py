43a057a78a3c891eddc7745314a09173e175d77e
marsnebulasoup <marsnebulasoup@gmail.com>
2022-05-21 10:39:59 -0500

    Does not build dashboard yet

--- serve_dashboard.py
+++ serve_dashboard.py
@@ -1,20 +1,35 @@
+from asyncio import subprocess
 import http.server
 import socketserver
+from subprocess import Popen, run
+from os.path import isdir
+import sys
 
-PORT = 8080
+API_PORT = 8000
+DASHBOARD_PORT = 8080
 
+print("Don't forget to run `npm run build` first...")
+try:
+  Popen(f'waitress-serve --port={API_PORT} --host=localhost api.app:app')
+except:
+  print("Unable to serve API.")
+  print(f"Make sure you don't already have something running on http://localhost:{API_PORT}")
+  sys.exit()
+
+# Serve dashboard
 try:
   class Handler(http.server.SimpleHTTPRequestHandler):
       def __init__(self, *args, **kwargs):
           super().__init__(*args, directory="dashboard/dist", **kwargs)
   Handler.extensions_map.update({
         ".js": "application/javascript",
-  });
+  })
 
 
-  httpd = socketserver.TCPServer(("", PORT), Handler)
+  httpd = socketserver.TCPServer(("", DASHBOARD_PORT), Handler)
 
-  print(f'Serving dashboard on http://localhost:{PORT}')
+  print(f'\n\n>>>>> ----- Serving dashboard on http://localhost:{DASHBOARD_PORT}\n\n')
   httpd.serve_forever()
 except:
-  print("Unable to serve dashboard. Did you build it? Make sure to run `npm run build` in the dashboard directory first.")
+  print("Unable to serve dashboard.")
+  print(f"Make sure you don't already have something running on http://localhost:{DASHBOARD_PORT}")
